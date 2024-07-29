c742141d6181be5f7ee466658ead24abfb5102ca
Lorenzo Nicora <1433255+nicusX@users.noreply.github.com>
2024-07-29 14:06:38 +0100

    Upgrade to Flink 1.19 and OpenSearch 2/13 (#1)
    * Upgrade to Flink 1.19 
    * Replaced ElasticSearch connector with latest OpenSearch connector
    * Default region changed to us-west-1

--- src/main/java/com/amazonaws/samples/msf/taxi/consumer/ProcessTaxiStreamLocal.java
+++ src/main/java/com/amazonaws/samples/msf/taxi/consumer/ProcessTaxiStreamLocal.java
@@ -15,12 +15,12 @@
 
 package com.amazonaws.samples.msf.taxi.consumer;
 
-import com.amazonaws.regions.Regions;
 import com.amazonaws.samples.msf.taxi.consumer.events.EventDeserializationSchema;
 import com.amazonaws.samples.msf.taxi.consumer.events.kinesis.Event;
 import com.amazonaws.samples.msf.taxi.consumer.events.kinesis.TripEvent;
 import com.amazonaws.samples.msf.taxi.consumer.utils.GeoUtils;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.kinesis.shaded.com.amazonaws.regions.Regions;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
@@ -33,55 +33,55 @@ import java.util.Properties;
 
 
 public class ProcessTaxiStreamLocal {
-  private static final Logger LOG = LoggerFactory.getLogger(ProcessTaxiStreamLocal.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ProcessTaxiStreamLocal.class);
 
-  private static final String DEFAULT_STREAM_NAME = "managed-flink-workshop";
-  private static final String DEFAULT_REGION_NAME = Regions.getCurrentRegion()==null ? "us-east-1" : Regions.getCurrentRegion().getName();
-
-
-  public static void main(String[] args) throws Exception {
-    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    private static final String DEFAULT_STREAM_NAME = "managed-flink-workshop";
+    private static final String DEFAULT_REGION_NAME = Regions.getCurrentRegion() == null ? "us-west-1" : Regions.getCurrentRegion().getName();
 
 
-    //read the parameters specified from the command line
-    ParameterTool parameter = ParameterTool.fromArgs(args);
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
 
-    Properties kinesisConsumerConfig = new Properties();
-    //set the region the Kinesis stream is located in
-    kinesisConsumerConfig.setProperty(AWSConfigConstants.AWS_REGION, parameter.get("Region", DEFAULT_REGION_NAME));
-    //obtain credentials through the DefaultCredentialsProviderChain, which includes credentials from the instance metadata
-    kinesisConsumerConfig.setProperty(AWSConfigConstants.AWS_CREDENTIALS_PROVIDER, "AUTO");
-    //poll new events from the Kinesis stream once every second
-    kinesisConsumerConfig.setProperty(ConsumerConfigConstants.SHARD_GETRECORDS_INTERVAL_MILLIS, "1000");
+        // Read the parameters specified from the command line
+        ParameterTool parameter = ParameterTool.fromArgs(args);
 
 
-    //create Kinesis source
-    DataStream<Event> kinesisStream = env.addSource(new FlinkKinesisConsumer<>(
-        //read events from the Kinesis stream passed in as a parameter
-        parameter.get("InputStreamName", DEFAULT_STREAM_NAME),
-        //deserialize events with EventSchema
-        new EventDeserializationSchema(),
-        //using the previously defined Kinesis consumer properties
-        kinesisConsumerConfig
-    ));
+        Properties kinesisConsumerConfig = new Properties();
+        // Set the region the Kinesis stream is located in
+        kinesisConsumerConfig.setProperty(AWSConfigConstants.AWS_REGION, parameter.get("Region", DEFAULT_REGION_NAME));
+        // Obtain credentials through the DefaultCredentialsProviderChain, which includes credentials from the instance metadata
+        kinesisConsumerConfig.setProperty(AWSConfigConstants.AWS_CREDENTIALS_PROVIDER, "AUTO");
+        // Poll new events from the Kinesis stream once every second
+        kinesisConsumerConfig.setProperty(ConsumerConfigConstants.SHARD_GETRECORDS_INTERVAL_MILLIS, "1000");
 
 
-    DataStream<TripEvent> trips = kinesisStream
-        //remove all events that aren't TripEvents
-        .filter(event -> TripEvent.class.isAssignableFrom(event.getClass()))
-        //cast Event to TripEvent
-        .map(event -> (TripEvent) event)
-        //remove all events with geo coordinates outside of NYC
-        .filter(GeoUtils::hasValidCoordinates);
+        // Create Kinesis source
+        DataStream<Event> kinesisStream = env.addSource(new FlinkKinesisConsumer<>(
+                // Read events from the Kinesis stream passed in as a parameter
+                parameter.get("InputStreamName", DEFAULT_STREAM_NAME),
+                // Deserialize events with EventSchema
+                new EventDeserializationSchema(),
+                // Using the previously defined Kinesis consumer properties
+                kinesisConsumerConfig
+        ));
 
 
-    //print trip events to stdout
-    trips.print();
+        DataStream<TripEvent> trips = kinesisStream
+                // Remove all events that aren't TripEvents
+                .filter(event -> TripEvent.class.isAssignableFrom(event.getClass()))
+                // Cast Event to TripEvent
+                .map(event -> (TripEvent) event)
+                // Remove all events with geo coordinates outside of NYC
+                .filter(GeoUtils::hasValidCoordinates);
 
 
-    LOG.info("Reading events from stream {}", parameter.get("InputStreamName", DEFAULT_STREAM_NAME));
+        // Print trip events to stdout
+        trips.print();
 
-    env.execute();
-  }
+
+        LOG.info("Reading events from stream {}", parameter.get("InputStreamName", DEFAULT_STREAM_NAME));
+
+        env.execute();
+    }
 }
---
+++ src/main/java/com/amazonaws/samples/msf/taxi/consumer/operators/AmazonOpenSearchServiceSink.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *    http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazonaws.samples.msf.taxi.consumer.operators;
+
+import io.github.acm19.aws.interceptor.http.AwsRequestSigningApacheInterceptor;
+import org.apache.flink.connector.opensearch.sink.OpensearchSink;
+import org.apache.flink.connector.opensearch.sink.OpensearchSinkBuilder;
+import org.apache.flink.connector.opensearch.sink.RestClientFactory;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.client.Requests;
+import org.opensearch.client.RestClientBuilder;
+import org.opensearch.common.xcontent.XContentType;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.signer.Aws4Signer;
+import software.amazon.awssdk.regions.Region;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * This class wraps the creation of an OpenSearch sink that writes JSON payload to Amazon OpenSearch service
+ */
+public class AmazonOpenSearchServiceSink {
+
+    private static IndexRequest createIndexRequest(String jsonElement, String index) {
+        return Requests.indexRequest()
+                .index(index)
+                .source(jsonElement, XContentType.JSON);
+    }
+
+    private static String extractHost(String endpointUrl) {
+        try {
+            URL openSearchEndpointUrl = new URL(endpointUrl);
+            return openSearchEndpointUrl.getHost();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Invalid endpoint URL: " + endpointUrl, e);
+        }
+    }
+
+    public static <IN> OpensearchSink<IN> buildAmazonOpenSearchSink(String index, String endpointUrl, RestClientFactory clientFactory) {
+        String host = extractHost(endpointUrl);
+
+        return new OpensearchSinkBuilder<IN>()
+                .setHosts(new HttpHost(host, 443, "https"))
+                .setAllowInsecure(true)
+                .setBulkFlushMaxActions(1) // disable bulk writes for simplicity
+                .setRestClientFactory(clientFactory)
+                .setEmitter((element, ctx, indexer) ->
+                        indexer.add(createIndexRequest(element.toString(), index)))
+                .build();
+    }
+
+    public static RestClientFactory createAmazonOpenSearchSigningRestClientFactory(String region) {
+        return new RestClientFactory() {
+            @Override
+            public void configureRestClientBuilder(RestClientBuilder restClientBuilder, RestClientConfig restClientConfig) {
+                // http client interceptor implementing request signing for Amazon OpenSearch Service
+                HttpRequestInterceptor interceptor = new AwsRequestSigningApacheInterceptor(
+                        "es",
+                        Aws4Signer.create(),
+                        DefaultCredentialsProvider.create(),
+                        Region.of(region));
+                restClientBuilder.setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.addInterceptorLast(interceptor));
+            }
+        };
+    }
+}
